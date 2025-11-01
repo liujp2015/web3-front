aed8409c7e17e1e5ff0e543239750c6c57888207
liujunpeng <392303104@qq.com>
2025-11-01 18:25:54 +0800

    init

---
+++ W1_D2/react-tic-tac-toe-game/app/components/Grid.tsx
@@ -0,0 +1,10 @@
+interface GridProps {}
+function Grid() {
+  return (
+    <div className="inline-block border p-10">
+      <button className="font-bold text-2xl w-5"></button>
+    </div>
+  );
+}
+
+export default Grid;
---
+++ W1_D2/react-tic-tac-toe-game/app/page.tsx
@@ -0,0 +1,26 @@
+import Grid from "./components/Grid";
+
+export default function Home() {
+  return (
+    // background Container  11122
+    <div className="flex justify-center items-center h-screen ">
+      {/**
+       *  Card Container
+       */}
+      <div className="bg-gray-500 p-4 mx-6 rounded-2xl shadow-8xl">
+        <h1 className="text-center text-3xl font-bold	">tic-tac-toe-game</h1>
+        <div className="grid  mt-5 p-12 grid-cols-3">
+          <Grid></Grid>
+          <Grid></Grid>
+          <Grid></Grid>
+          <Grid></Grid>
+          <Grid></Grid>
+          <Grid></Grid>
+          <Grid></Grid>
+          <Grid></Grid>
+          <Grid></Grid>
+        </div>
+      </div>
+    </div>
+  );
+}
