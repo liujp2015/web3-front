071af4ef3341513fe0e8f7cbcf7cf31678568cd4
liujunpeng <392303104@qq.com>
2025-11-01 18:50:01 +0800

    fix: bolding border css of nine grids

--- W1_D2/react-tic-tac-toe-game/app/components/Grid.tsx
+++ W1_D2/react-tic-tac-toe-game/app/components/Grid.tsx
@@ -1,10 +1,16 @@
-interface GridProps {}
-function Grid() {
+import clsx from "clsx";
+
+interface GridProps {
+  className?: string;
+  children?: React.ReactNode;
+}
+const Grid: React.FC<GridProps> = ({ className, children }) => {
   return (
-    <div className="inline-block border p-10">
+    <div className={clsx("inline-block border p-10", className)}>
       <button className="font-bold text-2xl w-5"></button>
+      {children}
     </div>
   );
-}
+};
 
 export default Grid;
--- W1_D2/react-tic-tac-toe-game/app/page.tsx
+++ W1_D2/react-tic-tac-toe-game/app/page.tsx
@@ -3,22 +3,22 @@ import Grid from "./components/Grid";
 export default function Home() {
   return (
     // background Container
-    <div className="flex justify-center items-center h-screen ">
+    <div className="flex justify-center items-center h-screen bg-gray-900">
       {/**
        *  Card Container
        */}
-      <div className="bg-gray-500 p-4 mx-6 rounded-2xl shadow-8xl">
+      <div className="bg-gray-100 p-4 mx-6 rounded-2xl shadow-8xl">
         <h1 className="text-center text-3xl font-bold	">tic-tac-toe-game</h1>
         <div className="grid  mt-5 p-12 grid-cols-3">
+          <Grid className=" border-l-2 border-t-2"></Grid>
+          <Grid className="border-t-2"></Grid>
+          <Grid className="border-t-2 border-r-2"></Grid>
+          <Grid className="border-l-2"></Grid>
           <Grid></Grid>
-          <Grid></Grid>
-          <Grid></Grid>
-          <Grid></Grid>
-          <Grid></Grid>
-          <Grid></Grid>
-          <Grid></Grid>
-          <Grid></Grid>
-          <Grid></Grid>
+          <Grid className="border-r-2"></Grid>
+          <Grid className="border-l-2 border-b-2"></Grid>
+          <Grid className="border-b-2"></Grid>
+          <Grid className="border-b-2 border-r-2"></Grid>
         </div>
       </div>
     </div>
