28078ac690cb73825c3e4a4b649d21fedceff417
liujunpeng <392303104@qq.com>
2025-11-09 10:38:38 +0800

    W1_D2

--- W1_D2/react-tic-tac-toe-game/app/page.tsx
+++ W1_D2/react-tic-tac-toe-game/app/page.tsx
@@ -2,11 +2,21 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>react井字游戏</h1>
-      <Link href="/component-version">父子组件版</Link>
-      <Link href="/context-version">上下文context版</Link>
-      <Link href="/redux-version">redux版</Link>
+    <div className="flex items-center justify-center flex-col ">
+      <h1 className="text-4xl mt-28 p-5 rounded-lg text-black bg-white">
+        react井字游戏
+      </h1>
+      <div className="flex flex-col gap-4 mt-5 text-2xl ">
+        <button className=" border p-5 rounded-lg hover:text-white hover:bg-blue-400 text-black bg-white">
+          <Link href="/component-version">父子组件版</Link>
+        </button>
+        <button className=" border p-5 rounded-lg hover:text-white hover:bg-blue-400 text-black bg-white">
+          <Link href="/context-version">上下文context版</Link>
+        </button>
+        <button className=" border p-5 rounded-lg hover:text-white hover:bg-blue-400 text-black bg-white">
+          <Link href="/redux-version">redux版</Link>
+        </button>
+      </div>
     </div>
   );
 }
